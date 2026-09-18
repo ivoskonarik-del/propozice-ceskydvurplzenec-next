e7a84a6f42bd3770c1b5605ab5a3d80e84956237
Weboviny Deploy <deploy@weboviny.cz>
2026-09-18 14:05:40 +0200

    Deploy 2026-09-18

---
+++ _next/static/chunks/app/akce-a-oslavy/page-9b109a0cf3e214d0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[208],{5580:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.bind(n,1787)),Promise.resolve().then(n.bind(n,4382))}},e=>{var s=s=>e(e.s=s);e.O(0,[377,382,787,441,517,358],()=>s(5580)),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/not-found-b1c77798db483b4b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[17,345,990],{9426:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.bind(n,4382))}},e=>{var s=s=>e(e.s=s);e.O(0,[377,382,441,517,358],()=>s(9426)),_N_E=e.O()}]);
